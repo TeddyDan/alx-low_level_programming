7aa75bb7dbec53381b719d4376c3991070ac972a
TeddyDan <muretidanteddy1@gmail.com>
2023-09-06 00:04:36 -0700

    alx-low_level_programming

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,47 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-  *str_concat - addition of a string
-  * @s1: first part
-  * @s2: second part
-  * Return: s1 + s2
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *add;
-	int a, b;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL || s2 == NULL)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
 	{
-		s1 = "";
-		s2 = "";
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
 	}
-	a = 0;
-	b = a;
-	while (s1[a] != '\0')
+
+	if (s2 != NULL)
 	{
-		a++;
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
 	}
-	while (s2[b] != '\0')
-	{
-		b++;
-	}
-	add = malloc(sizeof(char) * (a + b + 1));
-	if (add == NULL)
-	{
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
-	}
-	while (s1[a] != '\0')
-	{
-		add[a] = s1[a];
-		a++;
-	}
-	while (s2[b] != '\0')
-	{
-		add[a] = s2[b];
-		a++, b++;
-	}
-	add[a] = '\0';
-	return (add);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
